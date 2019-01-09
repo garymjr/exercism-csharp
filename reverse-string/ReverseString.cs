fce0d39f687716def85812987feb0b25e079d1b0
Gary Murray <gmurray@stotzeq.com>
2019-01-09 06:34:51 -0700

    Starting C# exercism track

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var charArr = input.ToCharArray();
+        Array.Reverse(charArr);
+        return new String(charArr);
+    }
+}
