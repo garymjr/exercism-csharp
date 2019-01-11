454b65fecca62d6d9febb676cf702371d71618af
Gary Murray <gmurray@stotzeq.com>
2019-01-11 08:03:04 -0700

    WIP: sum-of-multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var count = 0;
+        foreach (var multiple in multiples)
+        {
+            for (int i = 1; i < max; i++)
+            {
+                if (multiple % i == 0)
+                {
+                    count += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+}
